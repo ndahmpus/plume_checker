3c6ba0ffc861cffa96933daa4cfa1846945ee482
NdahMpus <fista010109@gmail.com>
2025-09-05 08:30:59 +0700

    Release v1.0.0: Added test files and updated screenshots to JPG format

---
+++ test/widget_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Portal Checker Tests', () {
+    test('Application should have correct name', () {
+      expect('Portal Checker', equals('Portal Checker'));
+    });
+  });
+}
