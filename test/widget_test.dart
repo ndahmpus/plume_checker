8e5c61ed6a9e73ffad9d748441847d58227a397b
NdahMpus <fista010109@gmail.com>
2025-09-05 09:10:04 +0700

    Upload plume_portal project to plume_checker repository (excluding large APK files)

--- test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  group('Portal Checker Tests', () {
-    test('Application should have correct name', () {
-      expect('Portal Checker', equals('Portal Checker'));
-    });
-  });
-}
